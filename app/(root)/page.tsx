1fdb5eb520cfe65002de7f1415b68240d6123687
Nishan Bangera <nishanbangera4321@gmail.com>
2024-12-30 18:21:43 +0530

    added card section in the home-page by making use of Card component from shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 const HomePage = () => {
-  return <>Shopping Mart</>;
+  return <>
+    <ProductList data={sampleData.products} title="Newest Arrivals" limit={4}/>
+  </>;
 }
  
 export default HomePage;
