5d353622cafd532d009191f6bdd3635c03c0e74e
Nishan Bangera <nishanbangera4321@gmail.com>
2025-01-01 10:09:26 +0530

    using zod defined product schema which can be reused on various type-definitions and also created a function in utils file to keep the decimal format in various scenarios

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,10 +1,11 @@
+import { Product } from "@/types";
 import ProductCard from "./product-card";
 
 const ProductList = ({
   data,
   title,
 }: {
-  data: any;
+  data: Product[];
   title?: string;
 }) => {
   return (
@@ -12,7 +13,7 @@ const ProductList = ({
       <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {data.map((product: any) => (
+          {data.map((product: Product) => (
             <ProductCard key={product.slug} product={product}/>
           ))}
         </div>
