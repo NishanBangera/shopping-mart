a304dcb8fe786ffe3e63729307deaa434618e69d
Nishan Bangera <nishanbangera4321@gmail.com>
2025-01-05 21:11:03 +0530

    created signIn page where most of the authentication part has been previously taken care in auth.ts file and made use of useActionState and useFormStatus hooks. Also added zod schema validation for signIn form

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,15 +1,21 @@
-import {z} from 'zod'
+import { z } from "zod";
 
 // Schema for inserting products
 export const insertProductSchema = z.object({
-    name: z.string().min(3, 'Name must be atleast 3 characters'),
-    slug: z.string().min(3, 'Slug must be atleast 3 characters'),
-    category: z.string().min(3, 'Category must be atleast 3 characters'),
-    brand: z.string().min(3, 'Brand must be atleast 3 characters'),
-    description: z.string().min(3, 'Description must be atleast 3 characters'),
-    stock: z.coerce.number(),
-    images: z.array(z.string()).min(1, 'Product must have atleast one image'),
-    isFeatured: z.boolean(),
-    banner: z.string().nullable(),
-    price: z.coerce.number()
-})
+  name: z.string().min(3, "Name must be atleast 3 characters"),
+  slug: z.string().min(3, "Slug must be atleast 3 characters"),
+  category: z.string().min(3, "Category must be atleast 3 characters"),
+  brand: z.string().min(3, "Brand must be atleast 3 characters"),
+  description: z.string().min(3, "Description must be atleast 3 characters"),
+  stock: z.coerce.number(),
+  images: z.array(z.string()).min(1, "Product must have atleast one image"),
+  isFeatured: z.boolean(),
+  banner: z.string().nullable(),
+  price: z.coerce.number(),
+});
+
+//Schema for signing users in
+export const signInFormSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be atleast 6 characters"),
+});
