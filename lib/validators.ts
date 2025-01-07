f6b2a5f79b0d407a7c88167104786fc1c180a7b3
Nishan Bangera <nishanbangera4321@gmail.com>
2025-01-07 17:10:08 +0530

    added zode schemas for cart and cartItem. Also created cart model which is then migrated to reflect in our db.

--- lib/validators.ts
+++ lib/validators.ts
@@ -26,9 +26,31 @@ export const signUpFormSchema = z
     name: z.string().min(3, "Name must be at least 3 characters"),
     email: z.string().email("Invalid email address"),
     password: z.string().min(6, "Password must be atleast 6 characters"),
-    confirmPassword: z.string().min(6, "Confirm Password must be atleast 6 characters"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirm Password must be atleast 6 characters"),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
     path: ["confirmPassword"],
   });
+
+// Cart Schemas
+export const cartItemSchema = z.object({
+  productId: z.string().min(1, "Product is required"),
+  name: z.string().min(1, "Name is required"),
+  slug: z.string().min(1, "Slug is required"),
+  qty: z.number().int().nonnegative("Quantity must be a positive number"),
+  image: z.string().min(1, "Image is required"),
+  price: z.coerce.number(),
+});
+
+export const insertCartSchema = z.object({
+  items: z.array(cartItemSchema),
+  itemsPrice: z.coerce.number(),
+  totalPrice: z.coerce.number(),
+  shippingPrice: z.coerce.number(),
+  taxPrice: z.coerce.number(),
+  sessionCartId: z.string().min(1, "Session cart id is required"),
+  userId: z.string().optional().nullable(),
+});
