400fa75329df81795f6c433df85aa320f40069f8
Nishan Bangera <nishanbangera4321@gmail.com>
2025-01-20 07:03:45 +0530

    worked on persisting of cart from guest account to original account

--- auth.ts
+++ auth.ts
@@ -2,8 +2,9 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
+import { cookies } from "next/headers";
 
 export const config = {
   pages: {
@@ -33,7 +34,10 @@ export const config = {
 
         //Check if user exists and if the password matches
         if (user && user.password) {
-          const isMatch = bcryptjs.compareSync(credentials.password as string, user.password)
+          const isMatch = bcryptjs.compareSync(
+            credentials.password as string,
+            user.password
+          );
 
           //If password is correct, return user
           if (isMatch) {
@@ -63,20 +67,53 @@ export const config = {
       }
       return session;
     },
-    async jwt({ token, user }: any) {
+    async jwt({ token, user, trigger, session }: any) {
       // Assign user fields to token
       if (user) {
+        token.id = user.id;
         token.role = user.role;
+
         // If user has no name then use the email
         if (user.name === "NO_NAME") {
           token.name = user.email!.split("@")[0];
+
           // Update database to reflect the token name
           await prisma.user.update({
             where: { id: user.id },
             data: { name: token.name },
           });
         }
+
+        if (trigger === "signIn" || trigger === "signUp") {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get("sessionCartId")?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              // Delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            }
+          }
+        }
       }
+
+      // Handle session updates
+      if (session?.user.name && trigger === "update") {
+        token.name = session.user.name;
+      }
+
       return token;
     },
   },
