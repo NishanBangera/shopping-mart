68fbfc02d5a338fd4c34bd47b0d087558b3f6f76
Nishan Bangera <nishanbangera4321@gmail.com>
2025-01-26 06:50:36 +0530

    refactored

--- auth.ts
+++ auth.ts
@@ -5,7 +5,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import bcryptjs from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
 import { cookies } from "next/headers";
-
+import { NextResponse } from "next/server";
 export const config = {
   pages: {
     signIn: "/sign-in",
@@ -60,7 +60,6 @@ export const config = {
       session.user.id = token.sub;
       session.user.role = token.role;
       session.user.name = token.name;
-
       // If there is an update, set the user name
       if (trigger === "update") {
         session.user.name = user.name;
@@ -116,6 +115,47 @@ export const config = {
 
       return token;
     },
+    authorized({ request, auth }: any) {
+      // Array of regex patterns of paths we want to protect
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      // Get pathname from the req URL object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and accessing a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
+      // Check for session cart cookie
+      if (!request.cookies.get('sessionCartId')) {
+        // Generate new session cart id cookie
+        const sessionCartId = crypto.randomUUID();
+
+        // Clone the req headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create new response and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+
+        // Set newly generated sessionCartId in the response cookies
+        response.cookies.set('sessionCartId', sessionCartId);
+
+        return response;
+      } else {
+        return true;
+      }
+    }
   },
 } satisfies NextAuthConfig;
 
