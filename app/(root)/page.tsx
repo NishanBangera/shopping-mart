b6dfce4c6f52aeb6d49ea9bf0ca95be07bcb73b3
Nishan Bangera <nishanbangera4321@gmail.com>
2025-02-03 16:27:33 +0530

    feat(homePage): implement carousel banner and menu drawer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import { getFeaturedProducts, getLatestProducts } from "@/lib/actions/product.actions";
 
 const HomePage = async() => {
   const latestProducts = await getLatestProducts()
+  const featuredProducts = await getFeaturedProducts()
   return <>
+    {featuredProducts.length && <ProductCarousel data={featuredProducts} />} 
     <ProductList data={latestProducts} title="Newest Arrivals"/>
   </>;
 }
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "./menu";
+import CategoryDrawer from "./category-drawer";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start">
+          <CategoryDrawer />
+          <Link href="/" className="flex-start ml-4">
             <Image
               src="/images/logo.svg"
               alt={`${process.env.NEXT_PUBLIC_APP_NAME} logo`}
